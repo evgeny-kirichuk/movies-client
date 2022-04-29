67c1ba3e4751a82b80535330aa3c399d25194d16
evgeny-kiricuk <zhenyakirichuk@gmail.com>
2022-04-29 19:26:17 +0200

    fix mobile styles

--- src/view/mainLayout/header/Header.tsx
+++ src/view/mainLayout/header/Header.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { ThemeSwitch } from '~molecules/themeSwitch/ThemeSwitch';
+import Button from '~atoms/button/Button';
 import { useLoggedIn } from '~slices/auth/selectors';
 import { Navigation } from '~organisms/navigation/Navigation';
 import { LoginButton } from '~organisms/login/LoginButton';
 import { SignUpButton } from '~organisms/signUp/SignUpButton';
 import { Logout } from '~organisms/logout/Logout';
+import { SortDownIcon } from '~icons/SortDownIcon';
 
 import styles from './Header.module.scss';
 
 export const Header: React.FC = () => {
+	const dropdownRef = useRef(null);
 	const isLoggedIn = useLoggedIn();
+	const [showMenu, setShowMenu] = useState(false);
+
+	useEffect(() => {
+		function handleClickOutside(event: Event) {
+			if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+				setShowMenu(false);
+			}
+		}
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [dropdownRef]);
 
 	return (
 		<header className={styles.headerWrapper}>
@@ -20,15 +37,42 @@ export const Header: React.FC = () => {
 					<span className={styles.js}>JS</span>
 				</a>
 				<Navigation />
-				{isLoggedIn ? (
-					<Logout />
-				) : (
-					<>
-						<SignUpButton />
-						<LoginButton />
-					</>
-				)}
-				<ThemeSwitch className={styles.switchStyle} />
+				<div className={styles.flatRightControls}>
+					{isLoggedIn ? (
+						<Logout />
+					) : (
+						<>
+							<SignUpButton />
+							<LoginButton />
+						</>
+					)}
+					<ThemeSwitch />
+				</div>
+				<div ref={dropdownRef} className={styles.hiddenRightControls}>
+					<Button
+						onClick={() => setShowMenu(!showMenu)}
+						style={{ transform: `rotate(${showMenu ? '180deg' : '0'})` }}
+					>
+						<SortDownIcon />
+					</Button>
+					<div
+						className={styles.controlsBlock}
+						style={{
+							height: showMenu ? '125px' : '0',
+							opacity: showMenu ? '1' : '0',
+						}}
+					>
+						{isLoggedIn ? (
+							<Logout />
+						) : (
+							<>
+								<SignUpButton />
+								<LoginButton />
+							</>
+						)}
+						<ThemeSwitch />
+					</div>
+				</div>
 			</div>
 		</header>
 	);
